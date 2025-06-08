fe02ecdb258f7dbde547c4b45a3266c69070defc
btrofimo <brendon@trofimowicz.com>
2025-06-07 23:58:10 -0500

    Add dependency setup and improve tooling

---
+++ SwiftApp/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:6.1
+import PackageDescription
+
+let package = Package(
+    name: "MESHMapMac",
+    platforms: [
+        .macOS(.v12)
+    ],
+    targets: [
+        .executableTarget(
+            name: "MESHMapMac",
+            dependencies: [],
+            resources: [
+                .copy("Resources/requirements.txt"),
+                .copy("Resources/install_python_deps.sh")
+            ],
+            .prebuildCommand(
+                displayName: "Install Python deps",
+                executable: .bash("Resources/install_python_deps.sh")
+            )
+        )
+    ]
+)
---
+++ SwiftApp/Resources/install_python_deps.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 -m pip install --user -r "$(dirname "$0")/requirements.txt"
